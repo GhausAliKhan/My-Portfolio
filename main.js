17b3ebf4f39b81a99d1162f5222eb703abcb19ab
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-06-29 00:29:09 +0500

    Add Popup Details Section to Portfolio "See Project" Button

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-const hamburger = document.querySelector('.hamburger');
-const toolbarMenu = document.querySelector('.toolbar-menu');
+const hamburger = document.querySelector(".hamburger");
+const toolbarMenu = document.querySelector(".toolbar-menu");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  toolbarMenu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  toolbarMenu.classList.toggle("active");
 });
 
-document.querySelectorAll('.menu-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  toolbarMenu.classList.remove('active');
-}));
+document.querySelectorAll(".menu-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    toolbarMenu.classList.remove("active");
+  })
+);
+
+const popupSection = document.querySelector(".popupSection");
+const X = document.querySelector(".X");
+const page = document.querySelector(".page");
+
+document.querySelectorAll(".seeProject").forEach((n) =>
+  n.addEventListener("click", () => {
+    popupSection.style.display = "block";
+    page.style.filter = "blur(5px)";
+  })
+);
+
+function disappear() {
+  popupSection.style.display = "none";
+  page.style.filter = "blur(0px)";
+}
+X.addEventListener("click", disappear);
+
+function disappearPage(event) {
+  if (event.target.className == "popupSection") {
+    popupSection.style.display = "none";
+    page.style.filter = "blur(0px)";
+  }
+}
+popupSection.addEventListener("click", disappearPage);
+
