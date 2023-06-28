cb44db99c5fe6954885b37eebcc3d6d31b88153c
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-06-29 03:10:51 +0500

    Fix Linter Errors

--- main.js
+++ main.js
@@ -1,49 +1,44 @@
-const hamburger = document.querySelector(".hamburger");
-const toolbarMenu = document.querySelector(".toolbar-menu");
+const hamburger = document.querySelector('.hamburger');
+const toolbarMenu = document.querySelector('.toolbar-menu');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  toolbarMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  toolbarMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".menu-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    toolbarMenu.classList.remove("active");
-  })
-);
+document.querySelectorAll('.menu-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  toolbarMenu.classList.remove('active');
+}));
 
-const popupSection = document.querySelector(".popupSection");
-const X = document.querySelector(".X");
-const page = document.querySelector(".page");
+const popupSection = document.querySelector('.popupSection');
+const X = document.querySelector('.X');
+const page = document.querySelector('.page');
 
-document.querySelectorAll(".seeProject").forEach((n) =>
-  n.addEventListener("click", () => {
-    popupSection.style.display = "block";
-    page.style.filter = "blur(5px)";
-  })
-);
+document.querySelectorAll('.seeProject').forEach((n) => n.addEventListener('click', () => {
+  popupSection.style.display = 'block';
+  page.style.filter = 'blur(5px)';
+}));
 
 function disappear() {
-  popupSection.style.display = "none";
-  page.style.filter = "blur(0px)";
+  popupSection.style.display = 'none';
+  page.style.filter = 'blur(0px)';
 }
-X.addEventListener("click", disappear);
+X.addEventListener('click', disappear);
 
 function disappearPage(event) {
-  if (event.target.className == "popupSection") {
-    popupSection.style.display = "none";
-    page.style.filter = "blur(0px)";
+  if (event.target.className === 'popupSection') {
+    popupSection.style.display = 'none';
+    page.style.filter = 'blur(0px)';
   }
 }
-popupSection.addEventListener("click", disappearPage);
-
+popupSection.addEventListener('click', disappearPage);
 
 const projects = [
   {
     id: 0,
     name: 'Multi-Post Stories <br> Gain+Glory',
-    description: `Some description`,
+    description: 'Some description',
     image: 'Assets/Img Plaholder.png',
     technology: ['Ruby', 'CSS', 'JavaScript', 'HTML'],
     source: '',
@@ -52,7 +47,7 @@ const projects = [
   {
     id: 1,
     name: 'Title 2',
-    description: `Some description`,
+    description: 'Some description',
     image: 'Assets/Img Plaholder.png',
     technology: ['Ruby', 'CSS', 'JavaScript', 'HTML'],
     source: '',
@@ -61,7 +56,7 @@ const projects = [
   {
     id: 2,
     name: 'Multi-Post Stories <br> Gain+Glory',
-    description: `Some description`,
+    description: 'Some description',
     image: 'Assets/Img Plaholder.png',
     technology: ['Ruby', 'CSS', 'JavaScript', 'HTML'],
     source: '',
@@ -70,7 +65,7 @@ const projects = [
   {
     id: 3,
     name: 'Multi-Post Stories <br> Gain+Glory',
-    description: `Some description`,
+    description: 'Some description',
     image: 'Assets/Img Plaholder.png',
     technology: ['Ruby', 'CSS', 'JavaScript', 'HTML'],
     source: '',
@@ -79,7 +74,7 @@ const projects = [
   {
     id: 4,
     name: 'Multi-Post Stories <br> Gain+Glory',
-    description: `Some description`,
+    description: 'Some description',
     image: 'Assets/Img Plaholder.png',
     technology: ['Ruby', 'CSS', 'JavaScript', 'HTML'],
     source: '',
@@ -88,7 +83,7 @@ const projects = [
   {
     id: 5,
     name: 'Multi-Post Stories <br> Gain+Glory',
-    description: `Some description`,
+    description: 'Some description',
     image: 'Assets/Img Plaholder.png',
     technology: ['Ruby', 'CSS', 'JavaScript', 'HTML'],
     source: '',
@@ -97,16 +92,7 @@ const projects = [
   {
     id: 6,
     name: 'Multi-Post Stories <br> Gain+Glory',
-    description: `Some description`,
-    image: 'Assets/Img Plaholder.png',
-    technology: ['Ruby', 'CSS', 'JavaScript', 'HTML'],
-    source: '',
-    live: '',
-  },
-  {
-    id: 7,
-    name: 'Last',
-    description: `Some description`,
+    description: 'Some description',
     image: 'Assets/Img Plaholder.png',
     technology: ['Ruby', 'CSS', 'JavaScript', 'HTML'],
     source: '',
@@ -114,66 +100,40 @@ const projects = [
   },
 ];
 
-const works = document.querySelector('.works');
+// const works = document.querySelector('.works');
+console.log(projects);
+const worksCard = document.createElement('div');
+worksCard.classList.add('works-card');
 
-const works_card = document.createElement('div');
-works_card.classList.add('works_card');
+// works.innerHTML = `
+// <div class="works_content">
+//   <h3>My Recent Works</h3>
+//   <img src="Assets/Indicator.png" alt="Indicator">
+// </div>
+// `;
 
-works.innerHTML = `
-<div class="works_content">
-  <h3>My Recent Works</h3>
-  <img src="Assets/Indicator.png" alt="Indicator">
-</div>
-`
+// works.appendChild(worksCard);
 
-works.appendChild(works_card)
+// let techList = "";
 
-let techList = '';
+// for (let i = 0; i < projects.length; i++) {
+//   techList = "";
+//   projects[i].technology.forEach((tech) => {
+//     techList += `<li>${tech}</li>`;
+//   });
+//   worksCard.innerHTML += `
+//   <div class="card">
+//     <div class="card_image">
+//         <img src="${projects[i].image}" alt="Image placeholder">
+//     </div>
+//     <div class="card_content">
+//         <h3>${projects[i].name}</h3>
+//         <ul>
+//             ${techList}
+//         </ul>
+//         <button class="seeProject">See Project</button>
+//     </div>
+// </div>
 
-
-for(let i = 0; i < projects.length; i++){
-  techList = '';
-  projects[i].technology.forEach(tech => {
-    techList += `<li>${tech}</li>`
-  })
-  works_card.innerHTML += `
-  <div class="card">
-    <div class="card_image">
-        <img src="${projects[i].image}" alt="Image placeholder">
-    </div>
-    <div class="card_content">
-        <h3>${projects[i].name}</h3>
-        <ul>
-            ${techList}
-        </ul>
-        <button class="seeProject">See Project</button>
-    </div>
-</div>
-
-  `
-}
-
-
-// projects.forEach(project => {
-//   techList = '';
-//   project.technology.forEach(tech => {
-//     techList += `<li>${tech}</li>`
-//   })
-//   works_card.innerHTML += `
-//                           <div class="card"
-//                             <div class="card_image">
-//                               <img src="${project.image}" alt="Image placeholder">
-//                             </div>
-//                               <div class="card_content">
-//                                 <h3>${project.name}</h3>
-//                                 <ul>
-//                                     ${techList}
-//                                 </ul>
-//                                 <button class="seeProject">See Project</button>
-//                               </div>  
-//                           </div>
-//                         `
-// })
-
-
-
+//   `;
+// }
