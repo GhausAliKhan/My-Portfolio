8b4a82fc9badfd680acf7ebc8ddaf27e49e11a68
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-06-29 20:19:28 +0500

    Fix Linter error

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ const inputList = document.querySelector('#inputList');
 const errorMessage = document.createElement('span');
 errorMessage.innerHTML = '*Your email should be in lower case';
 errorMessage.classList.add('errorMessage');
-contactForm.addEventListener("submit", (event) => {
+contactForm.addEventListener('submit', (event) => {
   if (email.value.toLowerCase() !== email.value) {
     event.preventDefault();
     inputList.appendChild(errorMessage);
